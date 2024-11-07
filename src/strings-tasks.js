5d09797b332fbea31cc9e05a4c5d4f5a8a06281a
Ronlnn <artking01kg@gmail.com>
2024-11-07 17:04:19 +0300

    feat: done sumOfCodes

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -192,8 +192,15 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  if (typeof str !== 'string') {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    sum += str.charCodeAt(i);
+  }
+  return sum;
 }
 
 /**
@@ -207,8 +214,8 @@ function sumOfCodes(/* str */) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith( str, subst) {
+  return str.startsWith()
 }
 
 /**
